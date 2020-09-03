dffcd35e5aaa37c98327a537173e80611ca8c051
HY <au_hui_yi@connect.ite.edu.sg>
2020-09-03 18:29:31 +0800

    v1.0

--- unitTest.py
+++ unitTest.py
@@ -4,11 +4,10 @@ import dataAnalysis as proj
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual([proj.top_3.index[0], proj.top_3.index[1], proj.top_3.index[2]], [' Indonesia ', ' Japan ', ' China '])
+        self.assertEqual([proj.top_3.index[0], proj.top_3.index[1], proj.top_3.index[2]], [' Australia ', ' USA ', ' New Zealand '])
 
     def test_somethings(self):
-        self.assertTrue([proj.top_3.index[0], proj.top_3.index[1], proj.top_3.index[2]], [' Indonesia ', ' Japan ', ' China '])
-
+        self.assertTrue([proj.top_3.index[0], proj.top_3.index[1], proj.top_3.index[2]], [' Australia ', ' USA ', ' New Zealand '])
 
 if __name__ == '__main__':
     unittest.main()
